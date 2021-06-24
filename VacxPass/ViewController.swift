abad5f98e5c9bfc70c5b04e2b602da95c9b93e5c
Nils Witt <nils@nils-witt.de>
2021-06-24 13:13:16 +0200

    Initial Commit

---
+++ VacxPass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VacxPass
+//
+//  Created by Nils Witt on 24.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
